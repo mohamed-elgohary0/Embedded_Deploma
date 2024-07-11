353566e15402b1b88f4c6e030fa75e332c5adca5
mohamed-elgohary0 <112410597+mohamed-elgohary0@users.noreply.github.com>
2024-07-11 19:42:30 +0300

    C_Programming

---
+++ Projects/C_Programming/C_Functions/Assignment/Ex1/Ex1.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : Ex1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int check(int i);
+
+int main(void) {
+	int i, n1, n2, count;
+	printf("Enter two numbers(intervals): ");
+	fflush(stdout);
+	scanf("%d %d", &n1, &n2);
+	printf("Prime numbers between %d and %d are: ", n1, n2 );
+	for(i= n1 + 1; i<n2; i++){
+		count = check(i);
+		if(count == 0)
+		printf("%d ", i);
+	}
+}
+int check(int i){
+	int count= 0;
+	int j;
+	for(j=2; j<i / 2; ++j){
+		if(i % j == 0){
+			count = 1;
+			break;
+		}
+	}
+	return count;
+}
+
