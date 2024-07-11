9e8b3a65db7f13ab458a76fc919cc30930d386b3
mohamed-elgohary0 <112410597+mohamed-elgohary0@users.noreply.github.com>
2024-07-11 18:54:29 +0300

    Ex2

---
+++ Projects/C_Programming/C_basics/Assignment_1/Ex2/Ex2.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int n;
+	printf("Enter an integer: ");
+	fflush(stdout);
+	scanf("%d",&n);
+	printf("Number is %d", n);
+}
